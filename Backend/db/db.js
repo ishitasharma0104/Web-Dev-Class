43121f1d30f779e1e323a7da321ce056fec6a78f
Ishita <ishita.iit.sharma@gmail.com>
2026-09-08 15:10:29 +0530

    Add backend and React project

---
+++ Backend/db/db.js
@@ -0,0 +1,16 @@
+let mongoose= require('mongoose')
+   
+let userSchema=  new mongoose.Schema({
+    name:String,
+    email:String,
+    passWord:String,
+    role:{
+        type:String,
+        emun:["user",'admin'],
+        default:"user"
+    }
+
+})
+
+let User= mongoose.model("user",userSchema)
+module.exports=User
---
+++ React/React/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Signup from "./Components/SignUp";
+import Login from "./Components/login";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
